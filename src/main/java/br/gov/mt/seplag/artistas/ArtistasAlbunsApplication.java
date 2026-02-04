044d9a57da2993cb58d9c05d07432a6a07e2aaee
Camargone <camargo65@gmail.com>
2026-02-03 22:43:28 -0400

    chore: inicializa projeto Spring Boot com configuração de profiles

---
+++ src/main/java/br/gov/mt/seplag/artistas/ArtistasAlbunsApplication.java
@@ -0,0 +1,12 @@
+package br.gov.mt.seplag.artistas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistasAlbunsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtistasAlbunsApplication.class, args);
+    }
+}
